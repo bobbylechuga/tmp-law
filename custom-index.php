47c6b72c5e5921ff169c1fdf6135c33ee49789dd
José Pino <jpino@tmpchile.cl>
2016-02-19 12:07:32 -0300

    covertido a páginas dinámicas todo el contenido del home

--- custom-index.php
+++ custom-index.php
@@ -28,46 +28,10 @@ get_header(); ?>
 			<div class="clearfix"></div>
 			</div>
 			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 welcome-right column">
-				<div class="row">
-					<span class="service-icon"><i class="icon-anulidad"></i></span>
-					<div class="service-content">
-						<h4>Legal Experties</h4>
-						<p class="hidden-xs hidden-sm">Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus.</p>
-						<a class="rmore-icon hidden-xs hidden-sm" href="#">View Practice Areas <i class="fa fa-caret-right"></i></a>
-					</div>
-				</div>	
-				<div class="row">
-					<span class="service-icon"><i class="icon-accidente_laboral"></i></span>
-					<div class="service-content">
-						<h4>Legal Experties</h4>
-						<p class="hidden-xs hidden-sm">Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus.</p>
-						<a class="rmore-icon hidden-xs hidden-sm" href="#">View Practice Areas <i class="fa fa-caret-right"></i></a>
-					</div>
-				</div>
-				<div class="row">
-					<span class="service-icon"><i class="icon-autodespido"></i></span>
-					<div class="service-content">
-						<h4>Legal Experties</h4>
-						<p class="hidden-xs hidden-sm">Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus.</p>
-						<a class="rmore-icon hidden-xs hidden-sm" href="#">View Practice Areas <i class="fa fa-caret-right"></i></a>
-					</div>
-				</div>
-				<div class="row">
-					<span class="service-icon"><i class="icon-despido_injustificado"></i></span>
-					<div class="service-content">
-						<h4>Legal Experties</h4>
-						<p class="hidden-xs hidden-sm">Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus.</p>
-						<a class="rmore-icon hidden-xs hidden-sm" href="#">View Practice Areas <i class="fa fa-caret-right"></i></a>
-					</div>
-				</div>
-				<div class="row">
-					<span class="service-icon"><i class="icon-tutela"></i></span>
-					<div class="service-content">
-						<h4>Legal Experties</h4>
-						<p class="hidden-xs hidden-sm">Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus.</p>
-						<a class="rmore-icon hidden-xs hidden-sm" href="#">View Practice Areas <i class="fa fa-caret-right"></i></a>
-					</div>
-				</div>
+				<?php query_posts('page_id=47'); ?>
+					<?php while (have_posts()) : the_post(); ?>
+						<?php the_content(); ?>
+				<?php endwhile; wp_reset_postdata(); ?>
 			</div>
 			<div class="clearfix"></div>
 		</div>
@@ -78,16 +42,12 @@ get_header(); ?>
 	<div class="container-fluid">
 		<div class="row">
 			<div class="callout-dark text-center fade-in-b">
-				<div class="call-contenido">
-					<h1><b>Call to action</b></h1>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ultrices eleifend elit, 
-						vel mollis felis egestas sed. Ut suscipit odio vel urna maximus ultricies. Sed est nisl, 
-						egestas a ultrices vitae, mattis ut quam. Cras at sodales ligula. Cum sociis natoque penatibus 
-						et magnis dis parturient montes, nascetur ridiculus mus. Integer feugiat ligula nec tellus 
-						vehicula congue. Donec vehicula suscipit lectus.
-					</p>
-					<p class="text-center"><a href="#" class="btn btn-default">+5 69 123456</a></p>
+				<div class="call-contenido">					
+					<?php query_posts('page_id=51'); ?>
+					<?php while (have_posts()) : the_post(); ?>
+						<h1><b><?php the_title(); ?></b></h1>
+						<?php the_content(); ?>
+					<?php endwhile; wp_reset_postdata(); ?>
 				</div>
 			</div>
 		</div>
@@ -95,6 +55,15 @@ get_header(); ?>
 	<!-- ./call  to action -->	
 
 	<!-- cases -->
+	<div class="cases page-builder-home">
+		<div class="container">
+			<?php query_posts('page_id=53'); ?>
+			<?php while (have_posts()) : the_post(); ?>
+				<?php the_content(); ?>
+			<?php endwhile; wp_reset_postdata(); ?>
+		</div>
+	</div>
+	<!--
 	<div class="cases">
 		<div class="container">
 			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
@@ -108,6 +77,7 @@ get_header(); ?>
 			<div class="clearfix"></div>
 		</div>
 	</div>
+	
 	<div class="cases">
 		<div class="container">
 			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
@@ -120,7 +90,7 @@ get_header(); ?>
 			</div>			
 			<div class="clearfix"></div>
 		</div>
-	</div>
+	</div>-->
 	<!-- cases-end -->
 	<!-- news -->
 	<!--
