cfa5806e2485540c11984471fdc55d87e99f6146
José Pino <pino.jose@gmail.com>
2016-02-22 13:04:01 -0300

    cambiado el slider como contenido de la página y no por template-part

--- template-parts/content-slide.php
+++ template-parts/content-slide.php
@@ -2,10 +2,32 @@
 	<div class="banner">
 		<div class="container">
 			<div class="banner-top">
-				<?php query_posts('page_id=85'); ?>
-				<?php while (have_posts()) : the_post(); ?>
-					<?php the_content(); ?>
-				<?php endwhile; wp_reset_postdata(); ?>
+				<section class="slider">
+					<div class="flexslider">
+						<ul class="slides">
+							<li>	
+								<div class="banner-text">
+									<h2>“WE OFFER OUR CLIENTS AN EXPERT LEGAL ADVICE!”</h2>
+									<p>Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English</p>
+									<a class="hvr-shutter-in-horizontal" href="#">Read More</a>
+								</div>
+							</li>
+							<li>	
+								<div class="banner-text">
+									<h2>“CLIENTS AN EXPERT WE OFFER OUR  LEGAL ADVICE!”</h2>
+									<p>Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English</p>
+									<a class="hvr-shutter-in-horizontal" href="#">Read More</a>
+								</div>
+							<li>	
+								<div class="banner-text">
+									<h2>“AN EXPERT LEGAL WE OFFER OUR CLIENTS  ADVICE!”</h2>
+									<p>Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English</p>
+									<a class="hvr-shutter-in-horizontal" href="#">Read More</a>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</section>			
 			</div>
 		</div>
 	</div>
